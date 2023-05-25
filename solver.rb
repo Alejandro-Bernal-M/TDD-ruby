1dc08315ba1f76d21eb81e2aeac95b90b4051eda
alejandro <alejandrober96@gmail.com>
2023-05-25 10:26:49 -0500

    factorial method implemented

--- solver.rb
+++ solver.rb
@@ -1,2 +1,13 @@
 class Solver
+
+  def factorial(n)
+    raise StandardError.new "No negative numbers" if n < 0
+    return 1 if n == 0
+    response = 1
+    while ( n > 0)
+      response = n * response
+      n = n -1
+    end
+    return response
+  end
 end
