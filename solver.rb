a201e4ff8477a36c31016f0eba3a833d92a3aed2
alejandro <alejandrober96@gmail.com>
2023-05-25 10:40:14 -0500

    Rubocop corrections

--- solver.rb
+++ solver.rb
@@ -1,29 +1,32 @@
+# frozen_string_literal: true
+
+# Class solver
 class Solver
-  def factorial(n)
-    raise ArgumentError, 'No negative numbers' if n < 0
-    return 1 if n == 0
+  def factorial(number)
+    raise ArgumentError, 'No negative numbers' if number.negative?
+    return 1 if number.zero?
 
     response = 1
-    while n > 0
-      response = n * response
-      n -= 1
+    while n.positive?
+      response = number * response
+      number -= 1
     end
     response
   end
 
-  def fizzbuzz(n)
-    if n % 15 == 0
-      "fizzbuzz"
-    elsif n % 3 == 0
-      "fizz"
-    elsif n % 5 == 0
-      "buzz"
+  def fizzbuzz(number)
+    if (number % 15).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
     else
-      n.to_s
+      number.to_s
     end
   end
 
-    def reverse(string)
-        string.reverse
-    end
+  def reverse(string)
+    string.reverse
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../solver'
 
 describe Solver do
@@ -15,31 +17,31 @@ describe Solver do
       expect { solver.factorial(-2) }.to raise_error('No negative numbers', ArgumentError)
     end
   end
+end
 
-  context "testing fizzbuzz method" do
-    it "When N is divisible by 3, return \"fizz\"" do
-      solver = Solver.new
-      solver.fizzbuzz(3).should == "fizz"
-    end
-    it "When N is divisible by 5, return \"buzz\"" do
-      solver = Solver.new
-      solver.fizzbuzz(5).should == "buzz"
-    end
-    it "When N is divisible by 3 and 5, return \"fizzbuzz\"" do
-      solver = Solver.new
-      solver.fizzbuzz(15).should == "fizzbuzz"
-    end
-    it "When N is not divisible by 3 nor 5, return \"N\"" do
-      solver = Solver.new
-      solver.fizzbuzz(7).should == "7"
-    end
+describe 'testing fizzbuzz method' do
+  it 'When N is divisible by 3, return "fizz"' do
+    solver = Solver.new
+    solver.fizzbuzz(3).should == 'fizz'
   end
-  
-  context 'When testing the reverse method' do 
-    it 'should return the reverse of the string' do 
-      solver = Solver.new
-      test_string = 'Hello World'
-      expect(solver.reverse(test_string)).to eq('dlroW olleH')
-    end
+  it 'When N is divisible by 5, return "buzz"' do
+    solver = Solver.new
+    solver.fizzbuzz(5).should == 'buzz'
+  end
+  it 'When N is divisible by 3 and 5, return "fizzbuzz"' do
+    solver = Solver.new
+    solver.fizzbuzz(15).should == 'fizzbuzz'
+  end
+  it 'When N is not divisible by 3 nor 5, return "N"' do
+    solver = Solver.new
+    solver.fizzbuzz(7).should == '7'
+  end
+end
+
+describe 'When testing the reverse method' do
+  it 'should return the reverse of the string' do
+    solver = Solver.new
+    test_string = 'Hello World'
+    expect(solver.reverse(test_string)).to eq('dlroW olleH')
   end
 end
