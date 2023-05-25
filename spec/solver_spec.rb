d541b55fe4203203ebb1e01a84cc9b3e4698821e
alejandro <alejandrober96@gmail.com>
2023-05-25 10:06:27 -0500

    Corrections on test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -12,7 +12,7 @@ describe Solver do
     end
 
     it 'Return error if negative number' do
-      expect(solver.factorial(-2)).to raise_error(Errors::NoNegativeNumbers)
+      expect{solver.factorial(-2)}.to raise_error
     end
   end
   
