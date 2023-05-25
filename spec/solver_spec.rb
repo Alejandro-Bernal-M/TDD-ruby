be5eafec5f87e9f36999686b134208ad7837d09f
sanieni6 <luissanchezzamora23@gmail.com>
2023-05-25 10:04:08 -0500

    Create test for the reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../solver'
 
 describe Solver do
-  
+  context 'When testing the reverse method' do 
+    it 'should return the reverse of the string' do 
+      solver = Solver.new
+      test_string = 'Hello World'
+      expect(solver.reverse(test_string)).to eq('dlroW olleH')
+    end
+  end
 end
