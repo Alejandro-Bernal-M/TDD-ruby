960e75c5df8e0689aea73306c55fc2c8c7a66430
alejandro <alejandrober96@gmail.com>
2023-05-25 10:00:23 -0500

    Test for factorial implemented

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,19 @@
 require_relative '../solver'
 
 describe Solver do
+  solver = Solver.new
+  context 'When usign factorial' do
+    it 'Return the correct factorial' do
+      expect(solver.factorial(4)).to eq(24)
+    end
+
+    it 'Return 1 if 0' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+
+    it 'Return error if negative number' do
+      expect(solver.factorial(-2)).to raise_error(Errors::NoNegativeNumbers)
+    end
+  end
   
 end
